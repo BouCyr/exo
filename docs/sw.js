1e75af515ddb7795e29aa0bfdef02e908c931fd3
Cyrille BOUCHER <cyrille.boucher@kleegroup.com>
2026-01-07 11:52:41 +0100

    wip

---
+++ docs/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'multiplication-v12';
+const ASSETS = [
+  './',
+  './index.html',
+  './ui.js',
+  './questions.js',
+  './main.js',
+  './manifest.json',
+  './icon.svg',
+  './config.json',
+  'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
